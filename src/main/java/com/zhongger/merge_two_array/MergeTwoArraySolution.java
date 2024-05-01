c8f016673980e7c0a9684433deadebaad72f01ed
Zhongger <zhongger@192.168.10.104>
2024-05-01 20:16:22 +0800

    合并两个有序数组

---
+++ src/main/java/com/zhongger/merge_two_array/MergeTwoArraySolution.java
@@ -0,0 +1,24 @@
+package com.zhongger.merge_two_array;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.cn/problems/merge-sorted-array/solutions/666608/he-bing-liang-ge-you-xu-shu-zu-by-leetco-rrb0/?envType=study-plan-v2&envId=top-interview-150
+ */
+public class MergeTwoArraySolution {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+        MergeTwoArraySolution.merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+}
